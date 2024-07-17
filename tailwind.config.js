9f5e388fa363f173f7fb27aa1f1daf99326bac54
Ahmad Feras <129334423+Ahmadfbq@users.noreply.github.com>
2024-07-17 19:11:51 +0300

    Adding the product List

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+import aspectRatio from '@tailwindcss/aspect-ratio';
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -21,4 +22,5 @@ export default {
     },
 
     plugins: [forms, typography],
+
 };
